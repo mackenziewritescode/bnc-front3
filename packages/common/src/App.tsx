de6a9948c574c1d8ffc364b3d60e8ec727b0d0bc
mackenzie <mackenzie@MacBook-Pro-de-CanAdmin.local>
2021-09-14 16:22:33 -0400

    going to try detecting teams a different way

--- packages/common/src/App.tsx
+++ packages/common/src/App.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { View, ActivityIndicator } from 'react-native'
+import * as msTeams from '@microsoft/teams-js'
 
-export function App() {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+import { Home } from './Home'
+import { Home as HomeTeams } from './Home.teams'
+
+export function App(): JSX.Element {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [onTeams, setOnTeams] = useState<boolean>(false)
+
+  // useEffect(() => {
+  //   async function initializeTeams(): Promise<null | undefined> {
+  //     try {
+  //       await msTeams.initialize(() => setOnTeams(true))
+  //     } catch (error) {
+  //       return null
+  //     } finally {
+  //       setLoading(false)
+  //     }
+  //   }
+  //   initializeTeams()
+  // }, [])
+
+  function renderHome() {
+    if (loading) {
+      return <ActivityIndicator />
+    }
+    if (onTeams) {
+      return <HomeTeams />
+    }
+    return <Home />
+  }
+
+  return <View>{renderHome}</View>
 }
