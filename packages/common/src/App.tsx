58bbc583f1ab4893f272189516ade8291bc147cf
mackenzie <mackenzie@MacBook-Pro-de-CanAdmin.local>
2021-09-15 13:45:21 -0400

    features/BNC-592. useTeams not running on mobile.

--- packages/common/src/App.tsx
+++ packages/common/src/App.tsx
@@ -1,36 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import { View, ActivityIndicator } from 'react-native'
-import * as msTeams from '@microsoft/teams-js'
+import { useTeams } from 'msteams-react-base-component'
 
 import { Home } from './Home'
 import { Home as HomeTeams } from './Home.teams'
 
 export function App(): JSX.Element {
   const [loading, setLoading] = useState<boolean>(true)
-  const [onTeams, setOnTeams] = useState<boolean>(false)
 
-  // useEffect(() => {
-  //   async function initializeTeams(): Promise<null | undefined> {
-  //     try {
-  //       await msTeams.initialize(() => setOnTeams(true))
-  //     } catch (error) {
-  //       return null
-  //     } finally {
-  //       setLoading(false)
-  //     }
-  //   }
-  //   initializeTeams()
-  // }, [])
+  const [{ inTeams }] = useTeams({})
+
+  useEffect(() => {
+    if (inTeams || inTeams !== undefined) {
+      setLoading(false)
+    }
+  }, [inTeams])
 
   function renderHome() {
     if (loading) {
       return <ActivityIndicator />
     }
-    if (onTeams) {
+    if (inTeams) {
       return <HomeTeams />
     }
     return <Home />
   }
 
-  return <View>{renderHome}</View>
+  return <View>{renderHome()}</View>
 }
