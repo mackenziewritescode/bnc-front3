4557ae7e09410c9eba5f29cb9d9c845227f2b806
mackenzie <mackenzie@MacBook-Pro-de-CanAdmin.local>
2021-09-15 15:09:02 -0400

    features/BNC-592

---
+++ packages/common/src/App.native.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+import { Home } from './Home'
+
+export function App(): JSX.Element {
+  return (
+    <View>
+      <Home />
+    </View>
+  )
+}
